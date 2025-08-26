89d62f2b45383129665f734ca1cfce5b3cedb67f
N1nsQ <nina.tulilahti@gmail.com>
2025-08-26 11:14:35 +0300

    remove field explanations (comments)

--- Server/WageWizard/WageWizard/Models/Payroll.cs
+++ Server/WageWizard/WageWizard/Models/Payroll.cs
@@ -5,23 +5,23 @@ namespace WageWizard.Models
     public class Payroll
     {
         public Guid Id { get; set; }
-        public string? Name { get; set; } // eg. "May 2025"
-        public DateTime? SalaryDate { get; set; } // eg. 15 May 2025
-        public DateTime? PayPeriod { get; set; } // eg. 1.5.2025 - 31.5.2025
+        public string? Name { get; set; }
+        public DateTime? SalaryDate { get; set; }
+        public DateTime? PayPeriod { get; set; }
 
         // Salary & Benefits
         public decimal? BaseSalary { get; set; }
         public decimal? MealBenefit { get; set; }
         public decimal? PhoneBenefit { get; set; }
         public decimal? OvertimePay { get; set; }
-        public decimal? GrossSalary { get; set; } // Kaikki palkat laskettuna yhteen, bruttopalkka
-        public decimal? NetSalary { get; set; } // Lopullinen maksettava summa työntekijälle
+        public decimal? GrossSalary { get; set; }
+        public decimal? NetSalary { get; set; }
 
         // Deductions 
-        public decimal? WithholdingTax { get; set; } // Ennakonpidätys
-        public decimal? EmployeePensionContribution { get; set; } // työntekijän TyEL-maksu
-        public decimal? UnemploymentInsuranceContribution { get; set; } // Työttyömyysvakuutusmaksu
-        public decimal? UnionMembershipFee { get; set; } // AY-jäsenmaksu
+        public decimal? WithholdingTax { get; set; }
+        public decimal? EmployeePensionContribution { get; set; }
+        public decimal? UnemploymentInsuranceContribution { get; set; }
+        public decimal? UnionMembershipFee { get; set; }
         
     }
 }
