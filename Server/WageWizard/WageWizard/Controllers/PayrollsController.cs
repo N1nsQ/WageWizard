4347d57f99c2436e3ff8ecb8bb44d913d8d5d322
N1nsQ <nina.tulilahti@gmail.com>
2025-08-26 10:02:00 +0300

    add Payrolls controller

---
+++ Server/WageWizard/WageWizard/Controllers/PayrollsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WageWizard.Models;
+
+namespace WageWizard.Controllers
+{
+    [ApiController]
+    [Route("api/Payrolls")]
+    public class PayrollsController : ControllerBase
+    {
+        private readonly PayrollContext _context;
+
+        public PayrollsController(PayrollContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Payroll>>> GetPayrolls()
+        {
+            return await _context.Payrolls.ToListAsync();
+        }
+    }
+}
