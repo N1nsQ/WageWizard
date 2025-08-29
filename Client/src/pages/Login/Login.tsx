05fc06942bbfd45f242ac8bcc167125b0c40ad30
N1nsQ <nina.tulilahti@gmail.com>
2025-08-29 09:19:33 +0300

    add Login-page

--- Client/src/pages/Login/Login.tsx
+++ Client/src/pages/Login/Login.tsx
@@ -32,53 +32,63 @@ export default function Login() {
   };
 
   return (
-    <Box
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      minHeight="50vh"
-      bgcolor="grey.100"
-    >
-      <Card sx={{ width: 400, borderRadius: 3, boxShadow: 4 }}>
-        <CardContent>
-          <Typography variant="h5" align="center" gutterBottom>
-            Kirjaudu sisään
-          </Typography>
-
-          {error && (
-            <Alert severity="error" sx={{ mb: 2 }}>
-              {error}
-            </Alert>
-          )}
-
-          <Box
-            component="form"
-            display="flex"
-            flexDirection="column"
-            gap={2}
-            onSubmit={handleLogin}
-          >
-            <TextField
-              label="Käyttäjätunnus"
-              variant="outlined"
-              fullWidth
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-            <TextField
-              label="Salasana"
-              type="password"
-              variant="outlined"
-              fullWidth
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <Button type="submit" variant="contained" color="primary" fullWidth>
+    <>
+      <div>
+        <Typography variant="h1">Wage Wizard</Typography>
+      </div>
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        minHeight="50vh"
+        bgcolor="grey.100"
+      >
+        <Card sx={{ width: 400, borderRadius: 3, boxShadow: 4 }}>
+          <CardContent>
+            <Typography variant="h5" align="center" gutterBottom>
               Kirjaudu sisään
-            </Button>
-          </Box>
-        </CardContent>
-      </Card>
-    </Box>
+            </Typography>
+
+            {error && (
+              <Alert severity="error" sx={{ mb: 2 }}>
+                {error}
+              </Alert>
+            )}
+
+            <Box
+              component="form"
+              display="flex"
+              flexDirection="column"
+              gap={2}
+              onSubmit={handleLogin}
+            >
+              <TextField
+                label="Käyttäjätunnus"
+                variant="outlined"
+                fullWidth
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              <TextField
+                label="Salasana"
+                type="password"
+                variant="outlined"
+                fullWidth
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                fullWidth
+              >
+                Kirjaudu sisään
+              </Button>
+            </Box>
+          </CardContent>
+        </Card>
+      </Box>
+    </>
   );
 }
