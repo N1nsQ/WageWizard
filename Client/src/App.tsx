859b54aa232ea79e8bc0df5f023c58aaf1084cd1
N1nsQ <nina.tulilahti@gmail.com>
2025-08-29 09:20:04 +0300

    add Routing

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,16 +1,27 @@
+// styles
 import "./App.css";
+
+// libraries
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// pages
+import { Home } from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import { Typography } from "@mui/material";
 
 function App() {
   return (
     <>
-      <div>
-        <Typography variant="h1">WageWizard</Typography>
-      </div>
-      <div>
-        <Login />
-      </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route
+            path="*"
+            element={<Typography variant="h3">PAGE NOT FOUND</Typography>}
+          />
+        </Routes>
+      </Router>
     </>
   );
 }
