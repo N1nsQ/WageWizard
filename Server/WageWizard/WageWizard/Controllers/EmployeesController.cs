be499299cbdb9753c22628e985f407b3b11813a6
N1nsQ <nina.tulilahti@gmail.com>
2025-09-22 23:30:42 +0300

    Employees symmary API call

--- Server/WageWizard/WageWizard/Controllers/EmployeesController.cs
+++ Server/WageWizard/WageWizard/Controllers/EmployeesController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using WageWizard.DTOs;
 using WageWizard.Models;
 
 namespace WageWizard.Controllers
@@ -10,11 +11,42 @@ namespace WageWizard.Controllers
     {
         private readonly PayrollContext _context = context;
 
-        [HttpGet]
+        [HttpGet()]
         public async Task<ActionResult<IEnumerable<Employee>>> GetEmployees()
         {
             return await _context.Employees.ToListAsync();
         }
-        
+
+        [HttpGet("summary")]
+        public async Task<ActionResult<IEnumerable<EmployeesListDto>>> GetEmployeesSummary()
+        {
+            
+
+            var employees = await _context.Employees
+                .OrderBy(e => e.LastName)
+                .Select(e => new EmployeesListDto
+                {
+                    FirstName = e.FirstName,
+                    LastName = e.LastName,
+                    JobTitle = e.JobTitle,
+                    ImageUrl = e.ImageUrl,
+                    Email = e.Email
+                })
+                .ToListAsync();
+
+            if (employees == null)
+            {
+                var error = new ErrorResponseDto
+                {
+                    Code = "backend_error_messages.employees_not_found"
+                };
+
+            return NotFound(error);
+
+            }
+
+            return Ok(employees);
+        }
+
     }
 }
---
+++ Server/WageWizard/WageWizard/DTOs/EmployeesListDto.cs
@@ -0,0 +1,11 @@
+﻿namespace WageWizard.DTOs
+{
+    public class EmployeesListDto
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? JobTitle { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? Email { get; set; }
+    }
+}
