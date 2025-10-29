150ffa217944bfce7a3822034ed30dbadb52f869
N1nsQ <nina.tulilahti@gmail.com>
2025-10-29 12:35:16 +0200

    Add TyEL Rates

---
+++ DB/2025_10_29_1229_CREATE_TABLE_TyEL_Rates.sql
@@ -0,0 +1,5 @@
+Create Table TyEL_Rates ( 
+Year int NOT NULL, 
+TyEL_Basic decimal NOT NULL,
+TyEL_Senior decimal NOT NULL
+);
---
+++ DB/2025_10_29_INSERT_INTO_TyEL_Rates.sql
@@ -0,0 +1,2 @@
+INSERT INTO TyEL_Rates(Year, TyEL_Basic, TyEL_Senior)
+Values(2025, 0.0715, 0.0865);
