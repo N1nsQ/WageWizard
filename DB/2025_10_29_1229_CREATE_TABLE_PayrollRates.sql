9a1d5716ab5f22b7005ede7ad6cf9a7176ab98d0
N1nsQ <nina.tulilahti@gmail.com>
2025-10-29 14:23:13 +0200

    change naming

--- DB/2025_10_29_1229_CREATE_TABLE_PayrollRates.sql
+++ DB/2025_10_29_1229_CREATE_TABLE_PayrollRates.sql
@@ -2,5 +2,5 @@ CREATE TABLE PayrollRates (
     Year INT NOT NULL PRIMARY KEY,
     TyEL_Basic DECIMAL(5,4) NOT NULL,
     TyEL_Senior DECIMAL(5,4) NOT NULL,
-    TVM DECIMAL(5,4) NOT NULL
+    UnemploymentInsurance DECIMAL(5,4) NOT NULL
 );
