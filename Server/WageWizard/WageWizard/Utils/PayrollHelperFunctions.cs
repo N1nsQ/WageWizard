a2a276adf2b4172bbf2824c62ad69171834c2b6c
N1nsQ <nina.tulilahti@gmail.com>
2025-11-04 14:31:20 +0200

    specify error messages

--- Server/WageWizard/WageWizard/Utils/PayrollHelperFunctions.cs
+++ Server/WageWizard/WageWizard/Utils/PayrollHelperFunctions.cs
@@ -9,7 +9,7 @@ namespace WageWizard.Utils
             if (age < 17 || age > 67)
                 return 0m;
 
-            var rates = context.PayrollRates.FirstOrDefault(r => r.Year == year) ?? throw new Exception($"TyEL rates not found for year {year}");
+            var rates = context.PayrollRates.FirstOrDefault(r => r.Year == year) ?? throw new KeyNotFoundException($"TyEL rates not found for year {year}");
            
             if (age >= 53 && age <= 62)
                 return rates.TyEL_Senior;
@@ -23,10 +23,9 @@ namespace WageWizard.Utils
             if (age < 18 || age >= 65)
                 return 0m;
 
-            var rates = context.PayrollRates.FirstOrDefault(r => r.Year == year) ?? throw new Exception($"TyEL rates not found for year {year}");
+            var rates = context.PayrollRates.FirstOrDefault(r => r.Year == year) ?? throw new KeyNotFoundException($"TyEL rates not found for year {year}");
 
             return rates.UnemploymentInsurance;
-
 
         }
     }
