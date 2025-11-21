b5585e60f59b5f068fdcd1602d3d7f23bf8841cc
N1nsQ <nina.tulilahti@gmail.com>
2025-11-21 13:45:53 +0200

    validate new employee (backend)

--- Server/WageWizard/WageWizard/DTOs/NewEmployeeRequestDto.cs
+++ Server/WageWizard/WageWizard/DTOs/NewEmployeeRequestDto.cs
@@ -1,18 +1,19 @@
-﻿namespace WageWizard.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WageWizard.DTOs
 {
-    // Testattu ja toimii
     public record NewEmployeeRequestDto(
-        string FirstName,
-        string LastName,
-        string JobTitle,
-        string Email,
-        string HomeAddress,
-        string PostalCode,
-        string City,
-        string BankAccountNumber,
-        decimal TaxRate,
-        decimal MonthlySalary,
-        DateTime StartDate,
-        DateTime DateOfBirth
+        [Required][StringLength(50, MinimumLength = 2)] string FirstName,
+        [Required][StringLength(50, MinimumLength = 2)] string LastName,
+        [Required][StringLength(100)] string JobTitle,
+        [Required][EmailAddress][StringLength(100)] string Email,
+        [Required][StringLength(200)] string HomeAddress,       
+        [Required][RegularExpression(@"^\d{5}$", ErrorMessage = "Postal code must be 5 digits.")] string PostalCode,
+        [Required][StringLength(50)] string City,
+        [Required] string BankAccountNumber,
+        [Required][Range(0, 100, ErrorMessage = "Tax rate must be between 0 and 100.")] decimal TaxRate,
+        [Required][Range(1, double.MaxValue, ErrorMessage = "Monthly salary must be greater than 0.")] decimal MonthlySalary,
+        [Required][DataType(DataType.Date)] DateTime StartDate,
+        [Required][DataType(DataType.Date)] DateTime DateOfBirth
         );
 }
