9b973a031e51d092c3a411a5b13304b5ec0db6df
N1nsQ <nina.tulilahti@gmail.com>
2025-11-21 14:43:37 +0200

    handle duplicate code

--- Server/WageWizard/WageWizard/Data/Repositories/FailingEmployeeRepository.cs
+++ Server/WageWizard/WageWizard/Data/Repositories/FailingEmployeeRepository.cs
@@ -7,16 +7,17 @@ namespace WageWizard.Data.Repositories
 {
     public class FailingEmployeeRepository : IEmployeeRepository
     {
+        private static readonly string ErrorMessage = "Database not reachable";
 
         public Task<EmployeeDto?> GetByIdAsync(Guid id)
-            => throw new RepositoryUnavailableException("Database not reachable");
+            => throw new RepositoryUnavailableException(ErrorMessage);
 
         public Task<IEnumerable<EmployeesSummaryDto>> GetEmployeesSummaryAsync()
-            => throw new RepositoryUnavailableException("Database not reachable");
+            => throw new RepositoryUnavailableException(ErrorMessage);
 
         public Task AddAsync(Employee employee)
-            => throw new RepositoryUnavailableException("Database not reachable");
+            => throw new RepositoryUnavailableException(ErrorMessage);
         public Task SaveChangesAsync()
-            => throw new RepositoryUnavailableException("Database not reachable");
+            => throw new RepositoryUnavailableException(ErrorMessage);
     }
 }
