8a6ad0a6e7069503ab4a399dbcec4c78107c676b
N1nsQ <nina.tulilahti@gmail.com>
2025-12-04 15:30:04 +0200

    language does not change anymore after refreshing page

--- Client/src/i18n.js
+++ Client/src/i18n.js
@@ -1,19 +1,29 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 
 import en from "./locales/en/translation.json";
 import fi from "./locales/fi/translation.json";
 
-i18n.use(initReactI18next).init({
-  resources: {
-    en: { translation: en },
-    fi: { translation: fi },
-  },
-  lng: "fi", // oletuskieli
-  fallbackLng: "en",
-  interpolation: {
-    escapeValue: false,
-  },
-});
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: { translation: en },
+      fi: { translation: fi },
+    },
+    // lng: "fi",
+    fallbackLng: "fi",
+
+    detection: {
+      order: ["localStorage", "navigator"],
+      caches: ["localStorage"],
+    },
+
+    interpolation: {
+      escapeValue: false,
+    },
+  });
 
 export default i18n;
