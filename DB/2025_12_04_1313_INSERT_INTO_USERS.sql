d073ad08c9fe426eb2d8b2a10e72becede652e75
N1nsQ <nina.tulilahti@gmail.com>
2025-12-04 13:15:26 +0200

    add users with different roles

---
+++ DB/2025_12_04_1313_INSERT_INTO_USERS.sql
@@ -0,0 +1,2 @@
+Insert into Users (Id, UserName, Email, Password, RoleId, IsActive)
+Values (NEWID(), 'Sirkku Näpsä', 'sirkku.napsa@dreamwork.com', 'Sirkku123', 3, 1);
---
+++ DB/2025_12_04_1314_INSERT_INTO_USERS.sql
@@ -0,0 +1,2 @@
+Insert into Users (Id, UserName, Email, Password, RoleId, IsActive)
+Values (NEWID(), 'Hessu Hopo', 'hessu.hopo@dreamwork.com', 'Hessu123', 2, 1);
